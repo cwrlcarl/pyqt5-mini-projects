21a01cf78e66f3e88eadb4c038b8dc91dc59be40
cwrl <29carlganalon@gmail.com>
2025-12-21 10:51:14 +0800

    Initial commit

--- poke-viewer/pokedex.py
+++ poke-viewer/pokedex.py
@@ -22,13 +22,13 @@ ASSETS_DIR = os.path.join(BASE_DIR, 'assets')
 class PokemonViewer(QWidget):
     def __init__(self):
         super().__init__()
-        self.setFixedSize(450, 530)
+        self.setFixedSize(700, 550)
         
         self.header = QLabel()
         self.input_pokemon = QLineEdit()
-        self.search_btn = QPushButton("Search")
         self.pokemon_img = QLabel()
         self.pokemon_name = QLabel(objectName="name")
+        self.pokemon_id = QLabel(objectName="id")
         self.pokemon_type = QLabel()
         self.pokemon_weight = QLabel()
         self.pokemon_height = QLabel()
@@ -41,20 +41,31 @@ class PokemonViewer(QWidget):
 
     
     def initUI(self):
-        pokedex_logo = QPixmap(os.path.join(ASSETS_DIR, 'pokedex_logo.png'))
-        self.header.setPixmap(pokedex_logo.scaled(193, 70, Qt.KeepAspectRatio,
+        image_width = int(self.width() * 0.2)
+        image_height = int(self.height() * 0.2)
+        pokedex_logo = QPixmap(os.path.join(ASSETS_DIR, 'pokemon_logo.png'))
+        self.header.setPixmap(pokedex_logo.scaled(image_width, image_height, Qt.KeepAspectRatio,
                                                   Qt.SmoothTransformation))
+        
         self.header.setAlignment(Qt.AlignHCenter)
         self.pokemon_img.setAlignment(Qt.AlignHCenter)
+        self.pokemon_type.setAlignment(Qt.AlignHCenter)
+
+        name_and_id = QHBoxLayout()
+        name_and_id.addWidget(self.pokemon_name)
+        name_and_id.addWidget(self.pokemon_id)
+        name_and_id.setAlignment(Qt.AlignHCenter)
 
         weight_and_height = QHBoxLayout()
         weight_and_height.addWidget(self.pokemon_weight)
         weight_and_height.addWidget(self.pokemon_height)
+        weight_and_height.setAlignment(Qt.AlignHCenter)
 
         pokemon_stats = QHBoxLayout()
         pokemon_stats.addWidget(self.hp_stat)
         pokemon_stats.addWidget(self.attack_stat)
         pokemon_stats.addWidget(self.defense_stat)
+        pokemon_stats.setAlignment(Qt.AlignHCenter)
 
         self.input_pokemon.setPlaceholderText("Search pokemon..")
         self.input_pokemon.returnPressed.connect(self.display_pokemon)
@@ -62,60 +73,62 @@ class PokemonViewer(QWidget):
         self.search_btn.setCursor(Qt.PointingHandCursor)
 
         search_field = QHBoxLayout()
+        search_field.addWidget(self.header)
         search_field.addWidget(self.input_pokemon)
-        search_field.addWidget(self.search_btn)
 
         widgets = [
-            self.header, search_field, self.pokemon_img,
-            self.pokemon_name, self.pokemon_type,
+            search_field, self.pokemon_img,
+            name_and_id, self.pokemon_type,
             weight_and_height, pokemon_stats
         ]
 
         horizontal_layouts = [
-            search_field, weight_and_height, pokemon_stats
+            search_field, name_and_id, weight_and_height, pokemon_stats
         ]
 
         main_layout = QVBoxLayout()
-        for widget in widgets:
+        for widget in widgets: 
             if widget in horizontal_layouts:
                 main_layout.addLayout(widget)
             else:
                 main_layout.addWidget(widget)
         self.setLayout(main_layout)
-        self.setContentsMargins(15, 15, 15, 15)
+        self.setContentsMargins(20, 15, 20, 15)
 
 
     def styleUI(self):
         self.setStyleSheet("""
             QWidget { 
+                margin: 2px 10px;
                 font-family: Poppins;
                 font-size: 15px;
-                background-color: white;
-            }
-                           
-            QLabel#name {
-
-            }
-                           
-            QLineEdit {
-                padding: 7px;
-                border: 1px solid #f2f2f2;
-                border-radius: 10px;
-                background-color: #f5f6f7;
-            }
-                           
-            QPushButton {
-                border-radius: 10px;
-                padding: 10px;
-                min-width: 130px;
-                color: #f5f6f7;
-                border: 1px solid #63aeff;
+                color: white;
                 background: qlineargradient(
                     x1: 0, y1: 0, 
                     x2: 1, y2: 1, 
-                    stop: 0.2 #63aeff,
-                    stop: 0.4 #8ac7ed,
-                    stop: 0.9 #0e4cc9
+                    stop: 0 #202224,
+                    stop: 0.3 #101114,
+                    stop: 1 #151617
+                );
+            }
+
+            QLabel {
+                background-color: transparent;              
+            }     
+                           
+            QLabel#name { font-size:25px; font-weight: Bold; }
+            QLabel#id { color: #bbbdbf; }
+                           
+            QLineEdit {
+                padding: 7px;
+                border: 1px solid #1e1f21;
+                border-radius: 10px;
+                background: qlineargradient(
+                    x1: 0, y1: 0, 
+                    x2: 1, y2: 1, 
+                    stop: 0 #202224,
+                    stop: 0.3 #1e1f21,
+                    stop: 1 #151617
                 );
             }
         """)
@@ -144,7 +157,7 @@ class PokemonViewer(QWidget):
             else:
                 print(f"Image HTTP Error: {response.status_code}")
                 return None
-        except requests.exception.RequestException as e:
+        except requests.exceptions.RequestException as e:
             print(f"Error fetching image: {e}")
             return None
 
@@ -168,18 +181,20 @@ class PokemonViewer(QWidget):
                                   Qt.SmoothTransformation)
                 )
 
-            name = pokemon_data['name'].capitalize()
-            types = pokemon_data['types'][0]['type']['name']
-            weight = pokemon_data['weight']
-            height = pokemon_data['height']
+            pokemon_name = pokemon_data['name'].capitalize()
+            pokemon_id = pokemon_data['id']
+            pokemon_type = pokemon_data['types'][0]['type']['name']
+            pokemon_weight = pokemon_data['weight']
+            pokemon_height = pokemon_data['height']
             hp = pokemon_data['stats'][0]['base_stat']
             attack = pokemon_data['stats'][1]['base_stat']
             defense = pokemon_data['stats'][2]['base_stat']
 
-            self.pokemon_name.setText(name)
-            self.pokemon_type.setText(types)
-            self.pokemon_weight.setText(f"Weight: {weight}")
-            self.pokemon_height.setText(f"Height: {height}")
+            self.pokemon_name.setText(pokemon_name)
+            self.pokemon_id.setText(f"#00{pokemon_id}")
+            self.pokemon_type.setText(pokemon_type)
+            self.pokemon_weight.setText(f"Weight: {pokemon_weight}")
+            self.pokemon_height.setText(f"Height: {pokemon_height}")
             self.hp_stat.setText(f"HP: {hp}")
             self.attack_stat.setText(f"Attack: {attack}")
             self.defense_stat.setText(f"Defense: {defense}")
